5c55d63cafbbf968fa440c597230d5a5d8499a81
boojo <jshim27@wisc.edu>
2025-12-22 18:32:57 -0600

    test update

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,7 +5,7 @@ def test_composer_fields():
     c = Composer(id=1, name="Johann Sebastian Bach",
                  birth="1685-01-01", death="1750-01-01", epoch="Baroque",
                  portrait="")
-    assert c.id == "1"
+    assert c.id == 1
     assert c.name == "Johann Sebastian Bach"
     assert c.birth == "1685-01-01"
     assert c.death == "1750-01-01"
@@ -15,6 +15,6 @@ def test_composer_fields():
 
 def test_work_fields():
     w = Work(id=1, title="Goldberg Variations, BWV.988", genre="Keyboard")
-    assert w.id == "1"
+    assert w.id == 1
     assert w.title == "Goldberg Variations, BWV.988"
     assert w.genre == "Keyboard"
